3da16551cccd58cb2d46be23cac199722b7e269b
Yesui <yesui1209@gmail.com>
2021-10-16 22:50:15 +0800

    react-bootstrap холболоо

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Alert from 'react-bootstrap/Alert'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      My blog
+      {
+        [
+          'primary',
+          'secondary',
+          'success',
+          'danger',
+          'warning',
+          'info',
+          'light',
+          'dark',
+        ].map((variant, idx) => (
+          <Alert key={idx} variant={variant}>
+            This is a {variant} alert—check it out!
+          </Alert>
+        ))
+      }
     </div>
   )
 }
