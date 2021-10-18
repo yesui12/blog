1d894ac9a07b8d4cf7bdee256655add17b4d1e6c
Yesui <yesui1209@gmail.com>
2021-10-18 15:24:11 +0800

    0.0.1 хувилбар гарлаа

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,10 @@ import {
   Card,
 } from "react-bootstrap";
 
-import MyNavbar from "../components/my-navbar";
+import MyNavbar from "components/my-navbar";
+//import Intro from "components/intro";
+import ListItem from "components/list-item";
+import GridItem from "components/grid-item";
 
 export default function Home() {
   return (
@@ -17,26 +20,8 @@ export default function Home() {
       <MyNavbar />
       <div className="blog-detail-page">
         <Row>
-          <Col md="8">
-            {/* <Media className="mb-4 admin-intro">
-              <Image
-                roundedCircle
-                width={64}
-                height={64}
-                className="mr-3"
-                src="logo.jpg"
-                alt="Generic placeholder"
-              />
-              <Media.Body>
-                <h5 className="font-weight-bold mb-0">
-                  1234.mn - Онлайн видео сургалт
-                </h5>
-                <p className="welcome-text">
-                  Бид програмчлалын технолгийн чиглэлээр төрөл бүрийн
-                  сонирхолтой мэдээллүүдийг энэхүү блогоор хүргэж байна.
-                </p>
-              </Media.Body>
-            </Media> */}
+          <Col md="12">
+            {/* <Intro /> */}
           </Col>
         </Row>
 
@@ -45,142 +30,33 @@ export default function Home() {
         <div className={`page-wrapper`}>
           <Row className="mb-5">
             <Col md="10">
-              <Card className={`fj-card fj-card-list`}>
-                <div className="card-body-wrapper">
-                  <Card.Header className="d-flex flex-row">
-                    <img
-                      src={"https://source.unsplash.com/user/erondu/150x150"}
-                      className="rounded-circle mr-3"
-                      height="50px"
-                      width="50px"
-                      alt="avatar"
-                    />
-                    <div>
-                      <Card.Title className="font-weight-bold mb-1">
-                        Амар Өсөхбаяр
-                      </Card.Title>
-                      <Card.Text className="card-date">
-                        2021 оны 3 сарын 21
-                      </Card.Text>
-                    </div>
-                  </Card.Header>
-                  <Card.Body>
-                    <Card.Title className="card-main-title">
-                      JAMstack архитектурын тухай
-                    </Card.Title>
-                    <Card.Text>Статик сайт</Card.Text>
-                  </Card.Body>
-                </div>
-              </Card>
+              <ListItem />
             </Col>
 
             <Col md="4">
-              <Card className={`fj-card`}>
-                <div className="card-body-wrapper">
-                  <Card.Header className="d-flex flex-row">
-                    <img
-                      src={"https://source.unsplash.com/user/erondu/150x150"}
-                      className="rounded-circle mr-3"
-                      height="50px"
-                      width="50px"
-                      alt="avatar"
-                    />
-                    <div>
-                      <Card.Title className="font-weight-bold mb-1">
-                        Амар Өсөхбаяр
-                      </Card.Title>
-                      <Card.Text className="card-date">
-                        2021 оны 3 сарын 2
-                      </Card.Text>
-                    </div>
-                  </Card.Header>
-                  <div className="view overlay">
-                    <Card.Img
-                      src="https://source.unsplash.com/user/erondu/250x250"
-                      alt="Card image cap"
-                    />
-                  </div>
-                  <Card.Body>
-                    <Card.Title className="card-main-title">
-                      Микросэрвис орчин үед
-                    </Card.Title>
-                    <Card.Text>Вэб технологи</Card.Text>
-                  </Card.Body>
-                </div>
-              </Card>
+              <GridItem />
             </Col>
 
             <Col md="4">
-              <Card className={`fj-card`}>
-                <div className="card-body-wrapper">
-                  <Card.Header className="d-flex flex-row">
-                    <img
-                      src={"https://source.unsplash.com/user/erondu/150x150"}
-                      className="rounded-circle mr-3"
-                      height="50px"
-                      width="50px"
-                      alt="avatar"
-                    />
-                    <div>
-                      <Card.Title className="font-weight-bold mb-1">
-                        Амар Өсөхбаяр
-                      </Card.Title>
-                      <Card.Text className="card-date">
-                        2021 оны 3 сарын 2
-                      </Card.Text>
-                    </div>
-                  </Card.Header>
-                  <div className="view overlay">
-                    <Card.Img
-                      src="https://source.unsplash.com/collection/190727/250x250"
-                      alt="Card image cap"
-                    />
-                  </div>
-                  <Card.Body>
-                    <Card.Title className="card-main-title">
-                      Монолотик архитектур
-                    </Card.Title>
-                    <Card.Text>Вэб технологи</Card.Text>
-                  </Card.Body>
-                </div>
-              </Card>
+              <GridItem />
             </Col>
 
             <Col md="4">
-              <Card className={`fj-card`}>
-                <div className="card-body-wrapper">
-                  <Card.Header className="d-flex flex-row">
-                    <img
-                      src={"https://source.unsplash.com/user/erondu/150x150"}
-                      className="rounded-circle mr-3"
-                      height="50px"
-                      width="50px"
-                      alt="avatar"
-                    />
-                    <div>
-                      <Card.Title className="font-weight-bold mb-1">
-                        Амар Өсөхбаяр
-                      </Card.Title>
-                      <Card.Text className="card-date">
-                        2021 оны 3 сарын 2
-                      </Card.Text>
-                    </div>
-                  </Card.Header>
-                  <div className="view overlay">
-                    <Card.Img
-                      src="https://source.unsplash.com/collection/190726/250x250"
-                      alt="Card image cap"
-                    />
-                  </div>
-                  <Card.Body>
-                    <Card.Title className="card-main-title">
-                      Sanity блог хийх
-                    </Card.Title>
-                    <Card.Text>Вэб технологи</Card.Text>
-                  </Card.Body>
-                </div>
-              </Card>
+              <GridItem />
             </Col>
+
+            <Col md="4">
+              <GridItem />
+            </Col>
+            
+            <Col md="4">
+              <GridItem />
+            </Col>
+            
+            <Col md="4">
+              <GridItem />
+            </Col>
+
           </Row>
         </div>
       </div>
